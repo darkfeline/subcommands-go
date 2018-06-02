64ebdf7fb16a3fdb2a81ad46bd8c1ad1dfc040b8
Allen Li <darkfeline@felesatra.moe>
2018-06-01 22:42:04 -0700

    Initial commit

---
+++ subcommands.go
@@ -0,0 +1,45 @@
+/*
+Package subcommands implements dead simple subcommands for Go.
+*/
+package subcommands
+
+import "fmt"
+
+// Cmd defines the interface for a subcommand to be used in Run.
+type Cmd interface {
+	Name() string
+	Run([]string)
+}
+
+// Command builds an object that implements Cmd.
+func Command(n string, f func([]string)) Cmd {
+	return &cmd{n, f}
+}
+
+type cmd struct {
+	name string
+	f    func([]string)
+}
+
+func (c *cmd) Name() string {
+	return c.name
+}
+
+func (c *cmd) Run(args []string) {
+	c.f(args)
+}
+
+// Run dispatches to the matching command using the arguments.  The
+// arguments slice should have at least one item.  The first string
+// should be the name of the subcommand.  An error is returned if no
+// subcommand is matched.
+func Run(c []Cmd, args []string) error {
+	n := args[0]
+	for _, c := range c {
+		if c.Name() == n {
+			c.Run(args)
+			return nil
+		}
+	}
+	return fmt.Errorf("Unmatched command %s", n)
+}
---
+++ subcommands_test.go
@@ -0,0 +1,51 @@
+package subcommands
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRunCallsWithSameArgs(t *testing.T) {
+	t.Parallel()
+	var args []string
+	f := func(a []string) {
+		args = a
+	}
+	c := []Cmd{
+		Command("foo", f),
+	}
+	a := []string{"foo", "bar"}
+	Run(c, a)
+	if !reflect.DeepEqual(a, args) {
+		t.Fatalf("Expected args %#v, got %#v", a, args)
+	}
+}
+
+func TestRunCallsRightCommandOnlyOnce(t *testing.T) {
+	t.Parallel()
+	var a, b, c int
+	fa := func([]string) {
+		a++
+	}
+	fb := func([]string) {
+		b++
+	}
+	fc := func([]string) {
+		c++
+	}
+	cmd := []Cmd{
+		Command("homura", fa),
+		Command("hikari", fb),
+		Command("pneuma", fc),
+	}
+	Run(cmd, []string{"hikari"})
+	if a != 0 {
+		t.Errorf("a was called")
+	}
+	if b != 1 {
+		t.Errorf("b was not called only once")
+	}
+	if c != 0 {
+		t.Errorf("c was called")
+	}
+}
